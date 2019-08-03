ee5d3626c0ea96a28bb99a578467b6a50e5836cb
Eric Hripko <yksirius@gmail.com>
2019-08-03 14:54:54 +0100

    Initial implementation for docker system info (#8)

--- system.go
+++ system.go
@@ -1,12 +1,19 @@
 package dipod
 
 import (
+	"net"
 	"net/http"
 	"runtime"
+	"time"
 
+	"github.com/docker/docker/api/types/registry"
 	"github.com/moby/moby/api/types"
 	log "github.com/sirupsen/logrus"
+
+	"github.com/EricHripko/dipod/iopodman"
 )
+
+const id = "5H6A:ME4Z:MBS5:AEUT:BDYB:MBHM:Y6UI:Y7CZ:DOGT:2CXX:D5RG:BKCP"
 
 // Ping is a handler function for /_ping.
 func Ping(res http.ResponseWriter, req *http.Request) {
@@ -32,4 +39,68 @@ func Version(res http.ResponseWriter, req *http.Request) {
 		Version:       ProxyVersion + "-dipod",
 	}
 	JSONResponse(res, ver)
+}
+
+// SystemInfo is a handler function for /info.
+func SystemInfo(res http.ResponseWriter, req *http.Request) {
+	log.Debug("system info")
+	backend, err := iopodman.GetInfo().Call(podman)
+	if err != nil {
+		WriteError(res, err)
+		return
+	}
+
+	info := types.Info{
+		Architecture:      backend.Host.Arch,
+		BridgeNfIP6tables: true,
+		BridgeNfIptables:  true,
+		CPUCfsPeriod:      true,
+		CPUCfsQuota:       true,
+		CPUSet:            true,
+		CPUShares:         true,
+		CgroupDriver:      "podman",
+		Containers:        int(backend.Store.Containers),
+		Debug:             false,
+		DockerRootDir:     backend.Store.Run_root,
+		Driver:            backend.Store.Graph_driver_name,
+		ID:                id,
+		IPv4Forwarding:    true,
+		Images:            int(backend.Store.Images),
+		KernelMemory:      true,
+		KernelVersion:     backend.Host.Kernel,
+		MemoryLimit:       true,
+		MemTotal:          backend.Host.Mem_total,
+		NCPU:              int(backend.Host.Cpus),
+		Name:              backend.Host.Hostname,
+		OSType:            backend.Host.Os,
+		OomKillDisable:    true,
+		OperatingSystem: (backend.Host.Distribution.Distribution + " " +
+			backend.Host.Distribution.Version),
+		RegistryConfig: &registry.ServiceConfig{},
+		ServerVersion:  backend.Podman.Podman_version,
+		SwapLimit:      true,
+		SystemTime:     time.Now().Format(time.RFC3339Nano),
+	}
+	info.DriverStatus = append(
+		info.DriverStatus,
+		[...]string{"Root Dir", backend.Store.Graph_root},
+		[...]string{"Options", backend.Store.Graph_driver_options},
+		[...]string{"Backing Filesystem", backend.Store.Graph_status.Backing_filesystem},
+		[...]string{"Supports d_type", backend.Store.Graph_status.Supports_d_type},
+		[...]string{"Native Overlay Diff", backend.Store.Graph_status.Native_overlay_diff},
+	)
+	for _, cidr := range backend.Insecure_registries {
+		_, ipnet, err := net.ParseCIDR(cidr)
+		if err != nil {
+			log.WithField("cidr", cidr).Warn("cidr parse fail")
+			continue
+		}
+
+		netipnet := registry.NetIPNet(*ipnet)
+		info.RegistryConfig.InsecureRegistryCIDRs = append(
+			info.RegistryConfig.InsecureRegistryCIDRs,
+			&netipnet,
+		)
+	}
+	JSONResponse(res, info)
 }
--- tests/cli/system.bats
+++ tests/cli/system.bats
@@ -20,3 +20,27 @@
     [[ "$(jq -r ".Details.Os" <<< $component)" == "linux" ]]
     [[ "$(jq -r ".Details.Arch" <<< $component)" == "amd64" ]]
 }
+
+@test "system: info" {
+    # Arrange/Act
+    output=$(docker system info --format '{{json .}}')
+    echo $output
+
+    # Assert
+    [[ "$(jq -r ".Driver" <<< $output)" == "overlay" ]]
+    [[ "$(jq -r ".MemoryLimit" <<< $output)" == "true" ]]
+    [[ "$(jq -r ".SwapLimit" <<< $output)" == "true" ]]
+    [[ "$(jq -r ".KernelMemory" <<< $output)" == "true" ]]
+    [[ "$(jq -r ".CpuCfsPeriod" <<< $output)" == "true" ]]
+    [[ "$(jq -r ".CpuCfsQuota" <<< $output)" == "true" ]]
+    [[ "$(jq -r ".CPUShares" <<< $output)" == "true" ]]
+    [[ "$(jq -r ".CPUSet" <<< $output)" == "true" ]]
+    [[ "$(jq -r ".IPv4Forwarding" <<< $output)" == "true" ]]
+    [[ "$(jq -r ".BridgeNfIptables" <<< $output)" == "true" ]]
+    [[ "$(jq -r ".BridgeNfIp6tables" <<< $output)" == "true" ]]
+    [[ "$(jq -r ".OomKillDisable" <<< $output)" == "true" ]]
+    [[ "$(jq -r ".CgroupDriver" <<< $output)" == "podman" ]]
+    [[ "$(jq -r ".OSType" <<< $output)" == "linux" ]]
+    [[ "$(jq -r ".Architecture" <<< $output)" == "amd64" ]]
+    [[ "$(jq -r ".DockerRootDir" <<< $output)" == "/var/run/containers/storage" ]]
+}
