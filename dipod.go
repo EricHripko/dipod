b579f4dbfa0ed79179a860b58944f4e35962b3d7
Eric Hripko <yksirius@gmail.com>
2020-02-29 11:23:46 +0000

    Better handle versioning (#34)

--- dipod.go
+++ dipod.go
@@ -12,8 +12,13 @@ const DockerUnixAddress = "/var/run/docker.sock"
 // PodmanUnixAddress is the default address of the Podman unix socket.
 const PodmanUnixAddress = "unix:///run/podman/io.podman"
 
-// APIVersion defines Docker Engine API version supported by this proxy.
+// APIVersion defines maximum Docker Engine API version supported by this
+// proxy.
 const APIVersion = "1.40"
+
+// MinAPIVersion defines minimum Docker Engine API version supported by this
+// proxy.
+const MinAPIVersion = "1.12"
 
 // ProxyVersion identified the dipod version.
 const ProxyVersion = "0.0.1"
